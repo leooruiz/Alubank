2cf36de33407b18e15513c61daa7739f1c2514ce
Leonardo <leoruizz14@outlook.com>
2023-10-30 22:23:20 -0300

    finish header

--- lib/components/sections/header.dart
+++ lib/components/sections/header.dart
@@ -6,17 +6,49 @@ class Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
-      child: const Row(
-        children: [
-          Column(
-            children: <Widget>[
-              Text("\$1000.00"),
-              Text("Balanço disponível"),
-            ],
-          ),
-          Icon(Icons.account_circle),
-        ],
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: <Color>[
+            Color.fromRGBO(103, 99, 234, 1),
+            Color.fromRGBO(155, 105, 254, 1),
+            Color.fromRGBO(195, 107, 255, 1),
+          ],
+        ),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(10),
+          bottomRight: Radius.circular(10),
+        ),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.fromLTRB(16, 80, 16, 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text.rich(TextSpan(
+                    text: "\$",
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: '1000.00',
+                          style: TextStyle(
+                              fontSize: 28, fontWeight: FontWeight.bold))
+                    ])),
+                Text("Balanço disponível", style: TextStyle(fontSize: 16)),
+              ],
+            ),
+            Icon(
+              Icons.account_circle,
+              size: 42,
+            ),
+          ],
+        ),
       ),
     );
   }
