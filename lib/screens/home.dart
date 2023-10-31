616b334fd8fa31c782233417df154d469261eb96
Leonardo <leoruizz14@outlook.com>
2023-10-30 21:25:18 -0300

    Create scaffold in Home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:alubank_alura_project/components/sections/header.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,6 +6,9 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Header(),
+    );
   }
 }
