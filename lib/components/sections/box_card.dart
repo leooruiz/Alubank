e848207b48948ffb9c31d7fdf20d63fdc3e04c27
Leonardo <leoruizz14@outlook.com>
2023-11-01 10:30:31 -0300

    add a box_card file

---
+++ lib/components/sections/box_card.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class BoxCard extends StatelessWidget {
+  const BoxCard({super.key, required this.boxContent});
+  final Widget boxContent;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Theme.of(context).cardColor,
+        boxShadow: kElevationToShadow[3],
+      ),
+      child: Column(children: [
+        const Row(
+          children: [Column(), Column()],
+        ),
+        const Text(""),
+        const LinearProgressIndicator(),
+        Container(),
+        const Text(""),
+        const Text("")
+      ]),
+    );
+  }
+}
