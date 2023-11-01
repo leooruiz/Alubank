b8f2bd50123ac823913308743dcc205a5c3c34d1
Leonardo <leoruizz14@outlook.com>
2023-11-01 10:31:28 -0300

    create my_theme file

---
+++ lib/themes/my_theme.dart
@@ -0,0 +1,8 @@
+import 'package:alubank_alura_project/themes/theme_colors.dart';
+import 'package:flutter/material.dart';
+
+ThemeData myTheme = ThemeData(
+  primarySwatch: ThemeColors.primaryColor,
+  primaryColor: ThemeColors.primaryColor,
+  brightness: Brightness.dark,
+);
