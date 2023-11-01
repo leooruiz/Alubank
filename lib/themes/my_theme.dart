95a7a6557538dcd204e8078f509f082e37ede9f1
Leonardo <leoruizz14@outlook.com>
2023-11-01 16:40:54 -0300

    add textTheme to my_theme

--- lib/themes/my_theme.dart
+++ lib/themes/my_theme.dart
@@ -5,4 +5,11 @@ ThemeData myTheme = ThemeData(
   primarySwatch: ThemeColors.primaryColor,
   primaryColor: ThemeColors.primaryColor,
   brightness: Brightness.dark,
+  textTheme: const TextTheme(
+    bodyMedium: TextStyle(fontSize: 16),
+    bodyLarge: TextStyle(
+      fontSize: 28,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
 );
